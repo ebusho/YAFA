86bb1041cbab9db542ef10f71fdcc455e915c3d2
Alis Adrović <alis.adrovic@gmail.com>
2025-07-28 19:43:20 +0200

    Add express webserver

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello, World!");
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to YAFA - Yet Another Finance App!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is up and running on port ${PORT}`);
+});
